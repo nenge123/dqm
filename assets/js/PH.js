3a50c73c846ba10b1f204ecec46b38983d232cea
nenge123 <m@nenge.net>
2023-03-30 08:30:02 +0800

    ok

---
+++ assets/js/PH.js
@@ -0,0 +1,42 @@
+var T = Nenge,I=T.I,F=T.F,urllist={},files={};
+function getUrl(path){
+    if(urllist[path])return urllist[path];
+    if(files[path]){
+        urllist[path] = F.URL(files[path],F.getExt(path));
+        delete files[path];
+        return urllist[path];
+    }
+    return path;
+}
+(async function(){
+    files = await Nenge.FetchItem({
+        url:'assets/data/pic.zip',
+        store:T.LibStore,
+        unpack:true,
+        process:e=>T.$('#status').innerHTML = e,
+    });
+    await T.addJS(`:root{
+        --pic-sl:url(${getUrl('pic/sl.gif')}) no-repeat;
+        --pic-ss:url(${getUrl('pic/ss.gif')}) no-repeat;
+        --pic-ml:url(${getUrl('pic/ml.gif')}) no-repeat;
+    }`+I.decode(files['style.min.css']),null,!0);
+    await T.addJS(files['dqm.min.js']);
+    delete files['style.min.css'];
+    delete files['dqm.min.js'];
+    T.$append(document.head,T.$ct('link',null,null,{
+        rel:"apple-touch-icon",
+        href:getUrl('dqm.png')
+    }));
+    T.$append(document.head,T.$ct('link',null,null,{
+        rel:"icon",
+        href:getUrl('dqm.png'),
+        type:"image/png"
+    }));
+    T.$('#status').hidden = true;
+    T.$('#content').hidden = false;
+    initSl();
+}).call(T);
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('assets/js/PH_sw.js').then(worker=>{
+    }).catch(e=>console.log('reg errot',e));
+}
---
+++ assets/js/PH_sw.js
@@ -0,0 +1,62 @@
+var CACHE_NAME = 'v1';
+var urlsToCache = [
+    '../../PH.html',
+    'common.min.js',
+    'PH.min.js',
+];
+const eventFunc = {
+    install(event){
+        console.log('install',event);
+        //self.skipWaiting();//跳过等待
+        event.waitUntil(
+            caches.open(CACHE_NAME).then(
+                cache=>cache.addAll(urlsToCache)
+            ).then(() => {
+                console.log('Cache downloaded',caches)
+                self.skipWaiting()
+            })
+        );
+    },
+    activate(event){
+        console.log('activate');
+        self.clients.claim();
+        event.waitUntil(
+            caches.keys().then(
+                cacheNames=>{
+                    return Promise.all(
+                        cacheNames.map(cacheName=>{
+                            console.log(cacheName);
+                            if(CACHE_NAME != cacheName){
+                                return caches.delete(cacheName);
+                            }
+                        })
+                    )
+                }
+            )
+        );
+    },
+    fetch(event){
+        console.log('fetch',event);
+        event.respondWith(
+            caches.match(event.request, {
+                ignoreSearch: true
+            }).then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                console.log('cache miss', event.request.url)
+                return fetch(event.request);
+            })
+        );
+    },
+    message(event){
+        console.log(event.data);
+    }
+};
+self.addEventListener('fetch',e=>console.log(e));
+Object.entries(eventFunc).forEach(
+    entry=>{
+        self.addEventListener(entry[0],event=>entry[1](event));
+    }
+);
